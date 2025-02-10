e9c72dfc1ab69d4ef30ae6503d4788268cedc4a9
jmsrf <jmseppala90@gmail.com>
2025-02-10 23:11:05 +0200

    Update flashcard_quiz to a version using functions

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -1,36 +1,46 @@
 from random import randint
 
-def print_randon_number()
+def generate_random_number():
+    """Generate a random number between 1 - 10."""
+    current_number = randint(1, 10)
+    print(f"DEBUG: Current number is {current_number}")  # Debugging info
+    return current_number # Return generated number
 
-	current_number = 0
+def ask_for_number(current_number): 
+    """
+    Ask user to give a number between 1 and 10.
+    Compares input with current_number and gives feedback until guessed correctly.
+    """
+    guess_number = 0
+    while True:
+        guess_number += 1 
+        print(f"DEBUG: Amount of guesses {guess_number}")
+        try:
+            answer = int(input("Please give a number between 1 - 10: "))
 
-	# Print random number between 1 and 10
-	current_number = randint(1, 10)
-	print("DEBUG: Current number is", current_number)
+            # If the number is in range 1 - 10, compare with current_number
+            if 1 <= answer <= 10:
+                if answer < current_number:
+                    print("Your guess is smaller than the current number. Try again!")      
+                elif answer > current_number:
+                    print("Your guess is bigger than the current number. Try again!")
+                else:
+                    print("You guessed the number right! Great job.")
+                    break # Exit loop when guessed correctly    
+            else:
+                # If input is a number but outside the valid range, print error message
+                print("Number is outside of range 1 - 10. Please try again!")
 
-while True:
-    try:
-        answer = int(input("Please give a number between 1 - 10: "))
-        
-        # If answer is in range 1 - 10 check how it compares to current number with the inner if/elif/else block.
-        if answer in range (1, 10): 
-        	if answer <  current_number:
-        		print("Your guess is smaller than the current number. Try again!")   	
-        	elif answer >  current_number:
-        		print("Your guess is bigger than the current number. Try again!")
-        	else:
-        		print("You guessed the number right! Great job.")
-        		break	
-        else:
-        	# If input is valid number but outside of 1 - 10, this gets executed
-            print("Number is outside of range 1 - 10. Please try again!")
-    except ValueError:
-    		# If input is not a number this gets executed.
-            print("Your input was not a number. Please give a number between 1 - 10:")
+        except ValueError:
+                # If input is not a number, print error message.
+                print("Your input was not a number. Please give a number between 1 - 10:")
 
+        if guess_number >= 3:
+            print("Maximum of 3 tries exceeded!")
+            break 
 
+# Generate a random number and pass it to the function
+random_number = generate_random_number() # Store the return value 
+ask_for_number(random_number)  # Pass it as an argument
 
-
-
-
-	   
+       
